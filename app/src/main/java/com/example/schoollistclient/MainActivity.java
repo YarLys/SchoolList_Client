cf252f43a6ed3ab813a92bad496beaef3753cf9a
YarLys <yar_lysakov@mail.ru>
2024-05-21 00:47:03 +0300

    Initial commit Простая разметка для navGraph'a. Дальше надо добавлять функционал

---
+++ app/src/main/java/com/example/schoollistclient/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.schoollistclient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+/*
+implementation("android.arch.navigation:navigation-fragment:1.0.0")
+    implementation("android.arch.navigation:navigation-ui:1.0.0")
+ */
