1d21fb33c612119c0f1455d1b70af98e90694e3e
YarLys <yar_lysakov@mail.ru>
2024-05-21 18:03:42 +0300

    Registration works!

--- app/src/main/java/com/example/schoollistclient/Register.java
+++ app/src/main/java/com/example/schoollistclient/Register.java
@@ -5,12 +5,21 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.example.schoollistclient.models.Teacher;
+import com.example.schoollistclient.retrofit.TeacherAPI;
+import com.example.schoollistclient.retrofit.retrofitService;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -68,13 +77,45 @@ public class Register extends Fragment {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (Validator.checkFirstName(view.findViewById(R.id.ETfname)) && Validator.checkSurname(view.findViewById(R.id.ETsurname))
-                    && Validator.checkPhone(view.findViewById(R.id.ETphone)) && Validator.checkEmail(view.findViewById(R.id.ETemail))
-                    && Validator.checkPassword(view.findViewById(R.id.ETpassword))) {
+                EditText first_name = view.findViewById(R.id.ETfname);
+                EditText surname = view.findViewById(R.id.ETsurname);
+                EditText last_name = view.findViewById(R.id.ETlname);
+                EditText phone = view.findViewById(R.id.ETphone);
+                EditText email = view.findViewById(R.id.ETemail);
+                EditText password = view.findViewById(R.id.ETpassword);
 
-                    Navigation.findNavController(view).navigate(R.id.action_register_to_profile);
-                    Toast.makeText(getContext(), "Вы успешно зарегистрированы!", Toast.LENGTH_LONG).show();
+                if (Validator.checkFirstName(first_name) && Validator.checkSurname(surname)
+                    && Validator.checkPhone(phone) && Validator.checkEmail(email) && Validator.checkPassword(password)) {
 
+                    retrofitService retrofitService = new retrofitService();
+                    TeacherAPI teacherAPI = retrofitService.getRetrofit().create(TeacherAPI.class);
+
+                    Teacher teacher = new Teacher();
+                    teacher.setFirst_name(first_name.getText().toString());
+                    teacher.setSurname(surname.getText().toString());
+                    teacher.setLast_name(last_name.getText().toString());
+                    teacher.setPhone(phone.getText().toString());
+                    teacher.setEmail(email.getText().toString());
+                    teacher.setPassword(password.getText().toString());
+                    teacherAPI.saveTeacher(teacher)
+                            .enqueue(new Callback<Teacher>() {
+                            @Override
+                            public void onResponse(Call<Teacher> call, Response<Teacher> response) {
+                                String response_code = response.toString().substring(response.toString().indexOf("code=")+5, response.toString().indexOf("code=")+8);
+                                Log.d("Response_Code", response_code);
+                                if (response_code.equals("200")) {
+                                    Navigation.findNavController(view).navigate(R.id.action_register_to_profile);
+                                    Toast.makeText(getContext(), "Вы успешно зарегистрированы!", Toast.LENGTH_LONG).show();
+                                }
+                                else { // если уже есть аккаунт с таким логином/телефоном
+                                    Toast.makeText(getContext(), "Ошибка сервера " + response_code, Toast.LENGTH_LONG).show();
+                                }
+                            }
+                            @Override
+                            public void onFailure(Call<Teacher> call, Throwable t) { // если что-то с подключением/самим сервером и тд
+                                Toast.makeText(getContext(), "Не удалось завершить регистрацию! Проблемы с сервером.", Toast.LENGTH_LONG).show();
+                                Log.d("SERVER_ERROR", ""+t);
+                            }});
                 }
             }
         });
